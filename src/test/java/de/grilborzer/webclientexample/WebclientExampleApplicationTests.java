aff26508d9f4e8ca3d89d530d5bdd1d33bb6fbf1
Sergej Grilborzer <sergej@grilborzer.de>
2023-06-01 11:26:39 +0200

    Initial commit

---
+++ src/test/java/de/grilborzer/webclientexample/WebclientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.grilborzer.webclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
